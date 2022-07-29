73b48884b7a10b27e0636248e6d30ef1a02001bd
Jarry Yang <260902108@qq.com>
2022-07-29 12:08:34 +0800

    Initial commit

---
+++ part1-oop/check-form-1.js
@@ -0,0 +1,11 @@
+// 验证姓名
+// function checkName() {}
+// // 验证邮箱
+// function checkEmail() {}
+// // 验证密码
+// function checkPassword() {}
+// 以上这种方式===下面这种方式 相当于声明三个全局变量
+var checkName = function () {};
+var checkEmail = function () {};
+var checkPassword = function () {};
+// 存在被覆盖的风险;
---
+++ part1-oop/check-form-2.js
@@ -0,0 +1,16 @@
+var CheckObj = {
+  checkName: function () {
+    console.log('check name');
+  },
+  checkEmail: function () {
+    console.log('check email');
+  },
+  checkPassword: function () {
+    console.log('check password');
+  },
+};
+
+// 使用
+CheckObj.checkName();
+CheckObj.checkEmail();
+CheckObj.checkPassword();
